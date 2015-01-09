8708f247abe0d6facb2b272a37d35422c6800682
Pete R. Jemian <prjemian@gmail.com>
2015-01-09 16:10:50 -0600

    refactored localConfig.py into XML file

---
+++ src/pv2web_ro/read_config.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+'''
+read XML configuration file for `pv2web_ro` package
+'''
+
+# Copyright (c) 2005-2015, UChicago Argonne, LLC.
+# See LICENSE file for details.
+
+
+import os
+import lxml.etree
+
+
+ROOT_TAG = 'pv2web_ro__config'
+
+
+def read_xml(xml_file):
+    '''return the configuration details as a dictionary'''
+    if not os.path.exists(xml_file):
+        raise IOError(xml_file + ' file not found')
+    tree = lxml.etree.parse(xml_file)
+    root = tree.getroot()
+    if root.tag != ROOT_TAG:
+        msg = 'XML root tag must be ' + ROOT_TAG
+        msg += ', found: ' + root.tag
+        raise ValueError(msg)
+    
+    conf = {}
+    for node in root:
+        key = node.tag
+        value = node.get('value')
+        if node.get('float', 'false').lower() == 'true':
+            value = float(value)
+        if node.get('int', 'false').lower() == 'true':
+            value = int(value)
+        conf[key] = value
+        
+    return conf

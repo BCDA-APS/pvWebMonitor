2c6baa411baf0c7d82e304563966f6da7560ee55
Pete Jemian <prjemian@gmail.com>
2015-01-13 18:11:23 -0600

    mark changes and bump version

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,5 +4,6 @@
 CHANGES
 =======
 
+:2015-01-12: 2015.0113.0 - validate all XML files and raise exceptions if invalid
 :2015-01-12: 2015.0112.2 - documentation at ReadTheDocs, package at PyPI, code at GitHub
 :2015-01-09: v1.0.0 - initial conversion from USAXS livedata project
--- src/pv2web_ro/__init__.py
+++ src/pv2web_ro/__init__.py
@@ -3,7 +3,7 @@ __package_name__        = u'pv2web_ro'
 __description__         = u'post EPICS PVs to read-only web page'
 __long_description__    = __description__
 
-__version__             = u'2015.0112.2'
+__version__             = u'2015.0113.0'
 __release__             = __version__
 __author__              = u'Pete R. Jemian'
 __email__               = u'jemian@anl.gov'
